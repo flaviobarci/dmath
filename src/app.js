314f716c60c10e439dab7f0f546aab5ddaecde20
Flavio Barci <flavio.barci@gmail.com>
2019-03-09 04:53:40 -0300

    add utils to convert string to set object

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@
 // Importing file system lib and shell interaction lib
 const fs = require("fs");
 const rl = require("readline-sync");
+const u = require("./utils")
+
 
 // Getting user input from argv
 const userArgs = process.argv.slice(2);
-const fileName = userArgs[0];
+const fileName = "test.txt"//userArgs[0];
 const filePath = process.cwd() + "/" + fileName;
 
 if (fileName === undefined) {
@@ -21,9 +23,23 @@ if (!fs.existsSync(filePath)) {
   process.exit(1);
 }
 
-console.log(fs.readFileSync(filePath, { encoding: "utf8" }));
+let raw = fs.readFileSync(filePath, { encoding: "utf8" }).toString();
 
-// while(true){
-// let something = rl.question("Say something: ");
-// console.log(something);
+console.log("File read.");
+
+let set = u.stringToSets(raw);
+
+console.log(set);
+
+// const setsExp = /[A-Z][ ]{1,}=[ ]{1,}{.*?}/g;
+
+// const setsArray = raw.match(setExp);
+
+// const elementExp = /[a-z][ ]{1,}=[ ]{1,}[0-9]{1,}/g;
+
+// const elementsArray = raw.match(elementExp);
+
+// while (true) {
+//   let something = rl.question("Say something: ");
+//   console.log(something);
 // }
---
+++ src/utils.js
@@ -0,0 +1,42 @@
+const stringToElement = setString => {
+  let nameExp = /[a-z]/g;
+  let name = setString.match(nameExp);
+  let valueExp = /-?[0-9]{1,}/g;
+  let value = setString.match(valueExp);
+  return { [name]: value };
+};
+
+const stringToSet = setString => {
+  let nameExp = /[A-Z]/g;
+  let name = setString.match(nameExp).toString();
+  let elementsExp = /-?[0-9]{1,}/g;
+  let elements = setString.match(elementsExp) || [];
+
+  return { [name]: elements };
+};
+
+const stringToSets = setsString => {
+  const setsExp = /[A-Z][ ]{1,}=[ ]{1,}{.*?}/g;
+
+  const setsArray = setsString.match(setsExp);
+
+  let sets = [];
+
+  for (arr in setsArray) {
+    sets.push(stringToSet(setsArray[arr]));
+  }
+
+  const elementExp = /[a-z][ ]{1,}=[ ]{1,}[0-9]{1,}/g;
+
+  const elementsArray = setsString.match(elementExp);
+
+  for (arr in elementsArray) {
+    sets.push(stringToElement(elementsArray[arr]));
+  }
+
+  return sets;
+};
+
+module.exports = {
+  stringToSets: stringToSets
+};
